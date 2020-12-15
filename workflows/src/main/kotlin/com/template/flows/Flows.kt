8d7a3c26cee7fd1116dbc8a684bc1c106d9422bc
thorgilman <thorgilman@gmail.com>
2020-12-15 16:45:02 -0500

    Basic round robin functionality complete

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -6,6 +6,7 @@ import com.template.states.DataState
 import net.corda.core.contracts.Command
 import net.corda.core.contracts.StateAndContract
 import net.corda.core.flows.*
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
@@ -14,31 +15,77 @@ import net.corda.core.utilities.ProgressTracker
 // *********
 // * Flows *
 // *********
+
+var index = 1
+
 @InitiatingFlow
 @StartableByRPC
-class Initiator(val dataString: String, val destParty: Party) : FlowLogic<SignedTransaction>() {
+class MyFlow(val dataString: String) : FlowLogic<SignedTransaction>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
     override fun call(): SignedTransaction {
 
+        // TODO: HEATH CHECK (through webserver?)
+
+        val destParty = getNextNode()
+
         val notary = serviceHub.networkMapCache.notaryIdentities.first()
 
         val command = Command(DataContract.Commands.Create(), listOf(ourIdentity).map { it.owningKey })
-        val dataState = DataState("Data", ourIdentity, destParty)
+        val dataState = DataState(dataString, ourIdentity, destParty)
         val stateAndContract = StateAndContract(dataState, DataContract.ID)
         val txBuilder = TransactionBuilder(notary).withItems(stateAndContract, command)
 
-        txBuilder.verify(serviceHub);
+        txBuilder.verify(serviceHub)
 
         val tx = serviceHub.signInitialTransaction(txBuilder)
         val targetSession = initiateFlow(destParty)
         return subFlow(FinalityFlow(tx, targetSession))
     }
+
+
+
+    // TODO: get configs / party map once and store in flow context ??? (for performance)
+    fun getNextNode(): Party {
+        // Get values from CorDapp Config
+        val config = serviceHub.getAppContext().config
+        val range = config.getInt("range")
+        val name = config.getString("name")
+
+        // Construct X500 Name
+        val x500Name = CordaX500Name.parse("OU=" + index + "," + name)
+        val party = serviceHub.networkMapCache.getPeerByLegalName(x500Name) ?: throw FlowException("No peer found with X500 name " + x500Name)
+
+        // Increment counter
+        if (++index > range) index = 1
+
+        return party
+    }
+
+    /*
+    fun getNodeList(): List<Party> {
+        val config = serviceHub.getAppContext().config
+
+        val range = config.getInt("range")
+        val name = config.getString("name")
+
+        val partyList = ArrayList<Party>()
+        for(i in 1..range) {
+            val newName = "OU=" + i + "," + name
+            val party = serviceHub.networkMapCache.getPeerByLegalName(CordaX500Name.parse(newName)) ?: throw FlowException("No peer found with X500 name " + newName)
+            partyList.add(party)
+        }
+        return partyList
+    }
+     */
+
+
+
 }
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
+@InitiatedBy(MyFlow::class)
+class MyFlowResponder(val counterpartySession: FlowSession) : FlowLogic<SignedTransaction>() {
     @Suspendable
     override fun call(): SignedTransaction {
 
--- workflows/src/test/kotlin/com/template/FlowTests.kt
+++ workflows/src/test/kotlin/com/template/FlowTests.kt
@@ -1,18 +1,16 @@
 package com.template
 
-import com.template.flows.Initiator
-import com.template.flows.Responder
+import co.paralleluniverse.fibers.Suspendable
+import com.template.flows.MyFlow
+import com.template.flows.MyFlowResponder
 import com.template.states.DataState
+import com.typesafe.config.ConfigFactory
 import net.corda.core.contracts.ContractState
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 import net.corda.core.node.services.queryBy
 import net.corda.core.utilities.getOrThrow
-import net.corda.testing.core.internal.ContractJarTestUtils.makeTestJar
-import net.corda.testing.node.MockNetwork
-import net.corda.testing.node.MockNetworkNotarySpec
-import net.corda.testing.node.MockNodeParameters
-import net.corda.testing.node.StartedMockNode
+import net.corda.testing.node.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -22,19 +20,27 @@ class FlowTests {
 
     lateinit var mockNetwork: MockNetwork
     lateinit var a: StartedMockNode
-    lateinit var b: StartedMockNode
+    lateinit var b1: StartedMockNode
+    lateinit var b2: StartedMockNode
+    lateinit var b3: StartedMockNode
+    lateinit var cordappConfig: Map<String, String>
 
     @Before
     fun setup() {
-        mockNetwork = MockNetwork(
-                listOf("com.template"),
-                notarySpecs = listOf(MockNetworkNotarySpec(CordaX500Name("Notary","London","GB")))
-        )
-        a = mockNetwork.createNode(MockNodeParameters())
-        b = mockNetwork.createNode(MockNodeParameters())
-        val startedNodes = arrayListOf(a, b)
+
+        cordappConfig = getCorDappConfig("workflows-0.1.conf")
+
+        mockNetwork = MockNetwork(MockNetworkParameters((listOf(TestCordapp.findCordapp("com.template.flows").withConfig(cordappConfig),
+                                                                TestCordapp.findCordapp("com.template.contracts")))))
+
+        a = mockNetwork.createNode(CordaX500Name.parse("O=MyParty,L=New York,C=US"))
+        b1 = mockNetwork.createNode(CordaX500Name.parse("OU=1,O=CentralNode,L=New York,C=US"))
+        b2 = mockNetwork.createNode(CordaX500Name.parse("OU=2,O=CentralNode,L=New York,C=US"))
+        b3 = mockNetwork.createNode(CordaX500Name.parse("OU=3,O=CentralNode,L=New York,C=US"))
+
+        val startedNodes = arrayListOf(a, b1, b2, b3)
         // For real nodes this happens automatically, but we have to manually register the flow for tests
-        startedNodes.forEach { it.registerInitiatedFlow(Responder::class.java) }
+        startedNodes.forEach { it.registerInitiatedFlow(MyFlowResponder::class.java) }
         mockNetwork.runNetwork()
     }
 
@@ -43,25 +49,32 @@ class FlowTests {
 
     }
 
+
     @Test
     fun `test`() {
-        val future = a.startFlow(Initiator("Data", b.identity()))
-        mockNetwork.runNetwork()
-
-        val tx = future.getOrThrow()
-
-        assert(a.getStates<DataState>().size == 1)
-        assert(b.getStates<DataState>().size == 1)
+        for (x in 1..5) {
+            val future = a.startFlow(MyFlow("Data"))
+            mockNetwork.runNetwork()
+            val tx = future.getOrThrow()
+        }
     }
 
 
-    fun StartedMockNode.identity(): Party {
+    private fun StartedMockNode.identity(): Party {
         return this.info.legalIdentities[0]
     }
 
-    inline fun <reified T: ContractState> StartedMockNode.getStates(): List<ContractState> {
+    private inline fun <reified T: ContractState> StartedMockNode.getStates(): List<ContractState> {
         return services.vaultService.queryBy<T>().states.map{it.state.data}
     }
 
+    private fun getCorDappConfig(configFileName: String): Map<String, String> {
+        val cordappConfig: MutableMap<String, String> = LinkedHashMap()
+        val config = ConfigFactory.parseResources(configFileName)
+        for ((key) in config.entrySet()) {
+            cordappConfig[key] = config.getString(key)
+        }
+        return cordappConfig
+    }
 
 }
